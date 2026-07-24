6179ef66de0512ad36eb94d12edd8f1d27b27725
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-07-24 22:54:59 +0530

    commented for admin only code from controller

--- ShoppingCart/Controllers/UserController.cs
+++ ShoppingCart/Controllers/UserController.cs
@@ -56,28 +56,28 @@ namespace ShoppingCart.Controllers
             return Ok(response);
         }
 
-        //For admin Only
-        [HttpGet]
-        [Route("Admins")]
-        [Authorize(Roles = "Admin")]
-        public IActionResult AdminEndPoint()
-        {
-            var currentUser = GetCurrentUser();
-            return Ok($"Hi you are an {currentUser.Role}"); 
-        }
-        private UserModel GetCurrentUser()
-        {
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-            if (identity != null)
-            {
-                var userClaims = identity.Claims;
-                return new UserModel
-                {
-                    Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
-                    Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
-                };
-            }
-            return null;
-        }
+        ////For admin Only
+        //[HttpGet]
+        //[Route("Admins")]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult AdminEndPoint()
+        //{
+        //    var currentUser = GetCurrentUser();
+        //    return Ok($"Hi you are an {currentUser.Role}"); 
+        //}
+        //private UserModel GetCurrentUser()
+        //{
+        //    var identity = HttpContext.User.Identity as ClaimsIdentity;
+        //    if (identity != null)
+        //    {
+        //        var userClaims = identity.Claims;
+        //        return new UserModel
+        //        {
+        //            Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+        //            Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+        //        };
+        //    }
+        //    return null;
+        //}
     }
 }
