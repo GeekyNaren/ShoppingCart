df1224129584706a8c456e951301e1ed926c1c7e
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-08-14 09:33:04 +0530

    Segregated auth controller and service

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 // Register HttpContextAccessor and current user service
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
