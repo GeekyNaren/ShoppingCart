13dc7ee28a1a0400b99fab4a793fb060c2c5bbab
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-08-16 12:49:05 +0530

    Added Roles constants

---
+++ ShoppingCart/Models/Dtos/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.Models.Dtos
+{
+    public static class Constants
+    {
+        public static class Roles
+        {
+            public const string Admin = "Admin";
+            public const string Customer = "Customer";
+        }
+    }
+}
