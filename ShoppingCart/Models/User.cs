6261777b10c720f21f89836b2418f853408e4f09
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-08-29 19:03:18 +0530

    modified user

--- ShoppingCart/Models/User.cs
+++ ShoppingCart/Models/User.cs
@@ -2,11 +2,11 @@
 {
     public class User
     {
-        public string Id { get; set; }  // MongoDB ObjectId as string
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; } // e.g. "Customer", "Admin"
+        public string? Id { get; set; }  // MongoDB ObjectId as string
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? Role { get; set; } // e.g. "Customer", "Admin"
         public DateTime CreatedAt { get; set; }
     }
 }
