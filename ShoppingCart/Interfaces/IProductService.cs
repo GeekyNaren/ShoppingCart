834187c560aea422a6e22a37dee5667c544d6505
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-09-23 20:12:21 +0530

    added product apis

--- ShoppingCart/Interfaces/IProductService.cs
+++ ShoppingCart/Interfaces/IProductService.cs
@@ -6,5 +6,9 @@ namespace ShoppingCart.Interfaces
     public interface IProductService
     {
         Task<ServiceResponse<bool>> AddProduct(AddProductDto request);
+        Task<ServiceResponse<List<ProductResponseDto>>> GetProducts();
+        Task<ServiceResponse<ProductResponseDto>> GetProductById(string id);
+        Task<ServiceResponse<bool>> UpdateProduct(UpdateProductDto request);
+        Task<ServiceResponse<bool>> DeleteProduct(string id);
     }
 }
---
+++ ShoppingCart/Models/Dtos/ProductResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace ShoppingCart.Models.Dtos
+{
+    public class ProductResponseDto
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+        public string ProductCategory { get; set; } = string.Empty;
+        public int ProductStockQuantity { get; set; }
+        public string ProductCategoryId { get; set; } = string.Empty;
+        public string ProductBrand { get; set; } = string.Empty;
+        public decimal? ProductDiscountPrice { get; set; }
+        public List<string> ProductImageUrls { get; set; } = [];
+        public double ProductAverageRating { get; set; }
+        public int ProductReviewCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
