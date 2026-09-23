a6d0f3be444c119c6c0879fdacb49a06b6ba5cec
Narendra Bhosale <bhosalenarendra15@gmail.com>
2026-09-23 21:06:43 +0530

    Added enums

--- ShoppingCart/Models/Dtos/Constants.cs
+++ ShoppingCart/Models/Dtos/Constants.cs
@@ -7,5 +7,31 @@
             public const string Admin = "Admin";
             public const string Customer = "Customer";
         }
+        public enum OrderStatus
+        {
+            Pending,
+            Processing,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+        public enum PaymentStatus
+        {
+            Pending,
+            Completed,
+            Failed,
+            Refunded
+        }
+        public enum ProductCategory
+        {
+            Electronics,
+            Clothing,
+            HomeAppliances,
+            Books,
+            Sports,
+            Beauty,
+            Toys,
+            Automotive
+        }
     }
 }
